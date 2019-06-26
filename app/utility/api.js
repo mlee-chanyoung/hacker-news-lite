7eab2f19ce188e22d73cd87a46b325f72c95a6cd
mclee3360 <mclee3360@gmail.com>
2019-06-25 20:52:59 -0400

    Create API for data retrieval

---
+++ app/utility/api.js
@@ -0,0 +1,82 @@
+export function getPostData(id)
+{
+    const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            return {
+                id: id,
+                title: data.title,
+                link: data.url === undefined ? "#" : data.url,
+                text: data.text,
+                metadata: {
+                    username: data.by,
+                    user_link: "#",
+                    date: data.time,
+                    comment_link: "#",
+                    comment_count: data.kids === undefined ? 0 : data.kids.length
+                }
+            }
+        })
+}
+
+export function getUserData(username)
+{
+    const url = `https://hacker-news.firebaseio.com/v0/user/${username}.json`;
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            return {
+                username: data.id,
+                joined: data.created,
+                karma: data.karma,
+                about: data.about,
+                posts: data.submitted
+            }
+        })
+}
+
+export function getCommentData(id)
+{
+    const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            return {
+                id: id,
+                text: data.text,
+                children: data.kids,
+                metadata: {
+                    username: data.by,
+                    user_link: "#",
+                    date: data.time
+                }
+            }
+        })
+}
+
+function fetchPosts(ids)
+{
+    return Promise.all(ids.map(id => getPostData(id)));
+}
+
+function fetchPostsByUrl(url)
+{
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            return fetchPosts(data.slice(0, 50));
+        });
+}
+
+export function fetchTopStories()
+{
+    const url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    return fetchPostsByUrl(url);
+}
+
+export function fetchNewStories()
+{
+    const url = "https://hacker-news.firebaseio.com/v0/newstories.json"
+    return fetchPostsByUrl(url);
+}
